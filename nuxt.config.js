12e90c2e9ef9387d74b032b1108a3396d204fce4
Isaque Silva <isaac@moonplug.agency>
2022-08-11 17:50:26 -0300

    Fixing responsiveness

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,9 @@ export default {
       // { hid: 'description', name: 'description', content: '' },
       // { name: 'format-detection', content: 'telephone=no' },
       { charSet:"UTF-8" },
-      { 'http-equiv':"X-UA-Compatible", content:"IE:edge" },
-      { name:"viewport", content:"width:device-width, initial-scale:1.0" },
-      { 'http-equiv':"X-UA-Compatible", content:"ie:edge" }
+      { 'http-equiv':"X-UA-Compatible", content:"IE=edge" },
+      { name:"viewport", content:"width=device-width, initial-scale=1.0" },
+      { 'http-equiv':"X-UA-Compatible", content:"ie=edge" }
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
